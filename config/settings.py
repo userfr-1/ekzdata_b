a88ad1be9ade1ab63fe9353c17f726b215fad0bc
user1 <qwerty@gmail.com>
2025-09-27 00:20:57 +0500

    add_st

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h7m%v3aw%t2@!awx5x7%cxi87&_9!26-1c3i=(mdit5r7th$39
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bsmvzq.pythonanywhere.com']
 
 
 # Application definition
@@ -116,6 +116,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/bsmvzq/ekzdata_b/staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
--- config/wsgi.py
+++ config/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+project_home = '/home/bsmvzq/ekzdata_b'
+if project_home not in sys.path:
+    sys.path.append(project_home)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
