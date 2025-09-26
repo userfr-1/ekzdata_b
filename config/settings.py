b2e19b655ab967869ca0012b90303a543c04ed8e
user1 <qwerty@gmail.com>
2025-09-27 00:33:29 +0500

    add_all_22

--- config/settings.py
+++ config/settings.py
@@ -115,12 +115,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 
+
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+from pathlib import Path
+import os
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
