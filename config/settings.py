c3b40dd1ac6d97857527fe56df643eb16481e0a3
user1 <qwerty@gmail.com>
2025-09-27 00:24:56 +0500

    oz

--- config/settings.py
+++ config/settings.py
@@ -116,10 +116,11 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/bsmvzq/ekzdata_b/staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
