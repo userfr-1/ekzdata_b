0225af4863dd5be311708347fcb379deb30d598a
user1 <qwerty@gmail.com>
2025-09-27 00:59:40 +0500

    all_f_b

--- config/wsgi.py
+++ config/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 import sys
 
-project_home = '/home/bsmvzq/ekzdata_b'
+project_home = '/home/bsmvzq/ekzdata'
 if project_home not in sys.path:
     sys.path.append(project_home)
 
